c3b2d6675841dbe5bd040e047cf3f0b3309f53c4
zhangfan <yoboseyo21@gmail.com>
2018-02-11 22:50:42 +0800

    update 页面title

--- src/router.js
+++ src/router.js
@@ -25,6 +25,7 @@ class Routes extends Component {
 			<Router>
 				<div style={{height: '100%'}}>
 					<Route path='/kaoya' render={()=>{
+							document.title = '除夕，我在约定之地等您。';
 							weChatShare({
 								title: '除夕，我在约定之地等您。',
 								desc: '除夕前夜，天空淅淅沥沥下起了雨，夜色像化不开的墨。忽然，你听到几声敲门——',
@@ -34,6 +35,7 @@ class Routes extends Component {
 						}}
 					/>
 					<Route path='/xiaolongxia' render={()=>{
+							document.title = '过年，和我待在家里！';
 							weChatShare({
 								title: '过年，和我待在家里！',
 								desc: '除夕市集里来来往往的人，叫卖声、说话声、鞭炮声震击你的耳膜，你不禁有些头痛。这时，走在前面的他忽然转过了头——',
@@ -43,6 +45,7 @@ class Routes extends Component {
 						}}
 					/>
 					<Route path='/zhutongfan' render={()=>{
+							document.title = '过年想只和你在一起，对，就你。';
 							weChatShare({
 								title: '过年想只和你在一起，对，就你。',
 								desc: '除夕夜，绚丽的烟花接连在头顶绽放，他的眼睛里有一闪一闪的小星星。你看着他，忽然很想伸手去摸摸他的头——',
@@ -52,6 +55,7 @@ class Routes extends Component {
 						}}
 					/>
 					<Route path='/qiudaoyu' render={()=>{
+							document.title = '嗯..守岁，猫咪也说要等你一起。';
 							weChatShare({
 								title: '嗯..守岁，猫咪也说要等你一起。',
 								desc: '年夜饭酒足饭饱后，席间还在热闹地讨论着什么。你看着他悄悄起身掩上了门，于是你也跟了上去——',
@@ -61,8 +65,9 @@ class Routes extends Component {
 						}}
 					/>
 					<Route path='/qiaokeli' render={()=>{
+							document.title = '明年，在我身边绽放笑颜吧。';
 							weChatShare({
-								title: '明年，在我身边绽放笑颜吧',
+								title: '明年，在我身边绽放笑颜吧。',
 								desc: '大年夜，你在等一个人。他说过会来，夜里风大，你披了件衣服站在门口期待着……那一抹熟悉的红色照亮你的眼瞳——',
 								imgUrl: window.location.origin + icon_qkl
 							});
@@ -70,6 +75,7 @@ class Routes extends Component {
 						}}
 					/>
 					<Route path='/guilinggao' render={()=>{
+							document.title = '别害怕，还有我在这里。';
 							weChatShare({
 								title: '别害怕，还有我在这里。',
 								desc: '除夕前日，周围的人走完了，只剩下你在宿舍里缓慢地整理东西。有一点迷茫，也有一点害怕……这时，身后传来一个熟悉的声音——',
@@ -79,6 +85,7 @@ class Routes extends Component {
 						}}
 					/>
 					<Route path='/yunwu' render={()=>{
+							document.title = '青山之中，与你静候新岁。';
 							weChatShare({
 								title: '青山之中，与你静候新岁。',
 								desc: '除夕之夜，山谷间悠扬的笛声响起，远远山脚下是此起彼此的烟火，对面坐着那个恬淡超然的她——曲毕，她开口道……',
@@ -88,6 +95,7 @@ class Routes extends Component {
 						}}
 					/>
 					<Route path='/cuyu' render={()=>{
+							document.title = '想要和您一起，看空中的繁花。';
 							weChatShare({
 								title: '想要和您一起，看空中的繁花。',
 								desc: '除夕一早，你在湖边遇到了新朋友，面前的少女脸色苍白，水珠顺着发丝一滴滴滑落。她静静地看着你几秒钟，忽然笑了出来。她向你伸出手——',
@@ -97,6 +105,7 @@ class Routes extends Component {
 						}}
 					/>
 					<Route path='/egan' render={()=>{
+							document.title = '在新旧交替的此刻，我们相遇了。';
 							weChatShare({
 								title: '在新旧交替的此刻，我们相遇了。',
 								desc: '准备年夜饭的时候，忽然一道光撒在你的面前。少女缓缓转过头来，逆着光，你看不清她脸上的表情。只能看见她手上光华流转，走近一看，原来是朵花。你听见面前的人对你说——',
@@ -106,6 +115,7 @@ class Routes extends Component {
 						}}
 					/>
 					<Route path='/futejia' render={()=>{
+							document.title = '除夕？...你得陪我喝到尽兴才行。';
 							weChatShare({
 								title: '除夕？...你得陪我喝到尽兴才行。',
 								desc: '除夕前夜，渐渐热闹起的小镇酒馆，提早下工的乡民陆续进屋，你的对面忽然坐下了一个有趣的姑娘。',
@@ -115,6 +125,7 @@ class Routes extends Component {
 						}}
 					/>
 					<Route path='/tilamisu' render={()=>{
+							document.title = '您的新年愿望，是什么呢？';
 							weChatShare({
 								title: '您的新年愿望，是什么呢？',
 								desc: '虽说是除夕，但还得工作。一下班你就往家里赶，赶得急了，你有些喘不上气，终于到家了还没敲门，门就开了——',
