7b93fa4c495991ce2686f4487ae826510bb4716a
张帆 <fan.zhang@mdslife.com>
2018-04-08 16:30:26 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Routes from './router.js';
+import Index from './container/index';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Routes />, document.getElementById('root'));
+ReactDOM.render(<Index />, document.getElementById('root'));
 registerServiceWorker();
